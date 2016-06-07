6a71c855e00f074945081d8f0a5fedb560b65e57
David Stapleton <david@platypi.io>
2016-06-07 11:11:44 -0500

    Initial commit

---
+++ happenTemplate.js
@@ -0,0 +1,86 @@
+var library = (function() {
+  return {
+	TimeStamp: (function(){
+   	  return {
+		UnixTimestamp: function(){},
+		UnixMillisecond: function(){}
+	  }
+	})(),
+	Local: (function(){
+	  return {
+		Time: (function() {
+		  return {
+	  	    WithSeconds: function(){},
+	   	    WithOutSeconds: function() {}
+		  }
+		})(),
+		MDY: (function(){
+	  	  return {
+		    Numeral: function(){},
+			Name: function(){}
+		  }
+		  })(),
+		}
+	})(),
+	Second: (function(){
+		return{
+			Second: function(){},
+			DblDigit: function(){}
+		}
+	})(),
+	Minute: (function(){
+		return{
+			Minute: function(){},
+			DblDigit: function(){}
+		}
+	})(),
+	Hour: (function(){
+		return {
+			TwentyFourHour: function() {},
+			TwelveHour: function() {},
+			AMPM: (function() {
+				return {
+					UpperCase: function(){},
+					LowerCase: function(){}
+				}
+			})()
+		}
+	})(),
+	Week: (function(){
+		return {
+			DayOfWeek: function(){},
+			AbrDayOfWeek: function(){},
+			FirstTwoOfWeek: function(){},
+			WeekOfYear: function(){}
+		}
+	})(),
+	Month: (function(){
+		return {
+			DateOfMonth: (function(){
+				return {
+					Numeral: function(){},
+					Ordinal: function(){},
+					DateDblDigit: function(){}
+				}
+			})(),
+			MonthNumber: function(){},
+			MonthNumberDblDigit: function(){},
+			AbrOfCurrentMonth: function(){},
+			CurrentMonth: function(){}
+		}
+	})(),
+	Year: (function(){
+		return {
+			DayOfYear: (function(){
+				return {
+					Numeral: function(){},
+					Ordinal: function(){}
+				}
+			})(),
+			YearFull: function(){},
+			YearAbr: function(){}
+		}
+	})(),
+	Defaults: function(){}
+  }
+})();
